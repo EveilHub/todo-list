1d3b63b9623438693adb936fa273f37f7d518911
TLRKiliann <philogenie@protonmail.com>
2026-01-24 13:16:44 +0100

    csv call to read data done

--- vite-project/src/components/FetchFromJson.tsx
+++ vite-project/src/components/FetchFromJson.tsx
@@ -3,41 +3,45 @@ import type { Todo } from "../lib/definitions";
 import "./styles/FetchFromJson.css";
 
 const FetchFromJson = (): JSX.Element => {
+
   const [todos, setTodos] = useState<Todo[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchTodos = async () => {
+    const fetchCSV = async (): Promise<void> => {
       try {
-        const res = await fetch("http://localhost:3001/api/todos");
-        if (!res.ok) throw new Error("Erreur serveur");
+        const res = await fetch("http://localhost:3001/api/todos/csv");
+        if (!res.ok) throw new Error("Erreur lors de la récupération des todos");
 
         const data: Todo[] = await res.json();
         setTodos(data);
-      } catch (err) {
-        setError("Impossible de charger les données");
+      } catch (err: any) {
+        setError(err.message);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchTodos();
+    fetchCSV();
+    return () => console.log("Clean-up CSV");
   }, []);
 
-  const deleteDataJson = async (id: string): Promise<void> => {
-    setTodos(todos.filter((todo: Todo) => (todo.id !== id)));
-    try {
-      await fetch(`http://localhost:3001/api/todos/${id}`, {
-        method: "DELETE",
-      });
-    } catch (error) {
-      console.error("Erreur suppression serveur", error);
-    }
-  };
+  // const deleteDataJson = async (id: string): Promise<void> => {
+  //   setTodos(todos.filter((todo: Todo) => (todo.id !== id)));
+  //   try {
+  //     await fetch(`http://localhost:3001/api/todos/${id}`, {
+  //       method: "DELETE",
+  //     });
+  //   } catch (error) {
+  //     console.error("Erreur suppression serveur", error);
+  //   }
+  // };
 
   if (loading) return <h3>Chargement...</h3>;
   if (error) return <h3>{error}</h3>;
+
+  console.log(todos.map((x) => x));
 
   return (
     <div className="div--fetch">
@@ -60,7 +64,7 @@ const FetchFromJson = (): JSX.Element => {
               <div>
                 <button
                   type="button" 
-                  onClick={() => deleteDataJson(todo.id)} 
+                  // onClick={() => deleteDataJson(todo.id)} 
                   className="custom-btn"  
                 >
                   Delete
