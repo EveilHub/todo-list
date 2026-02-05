31777767d3dee4bbc523af134186c2becf7937bd
EveilHub <EveilHub@proton.me>
2026-02-05 11:41:04 +0100

    table of titles done

--- vite-project/src/components/TableOfTodos.tsx
+++ vite-project/src/components/TableOfTodos.tsx
@@ -5,16 +5,16 @@ const TableOfTodos = (): JSX.Element => {
     return (
         <div className="table--todos">
             <div className="div--title--projects">
-                <h3 className="h3--table">Projets</h3>
+                <h3 className="h3--table t-one">Projets</h3>
             </div>
             <div className="div--title--tasks">
-                <h3 className="h3--table">Tâches</h3>
+                <h3 className="h3--table t-two">Tâches</h3>
             </div>
             <div className="div--title--delays">
-                <h3 className="h3--table">Délais</h3>
+                <h3 className="h3--table t-three">Délais</h3>
             </div>
             <div className="div--title--clients">
-                <h3 className="h3--table">Clients</h3>
+                <h3 className="h3--table t-four">Clients</h3>
             </div>
             
         </div>
